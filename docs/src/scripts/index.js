514f9522968965af02dcc840796d7453f4e8fe6d
Alexandrshy <alexandrshy@gmail.com>
2020-08-08 01:54:27 +0400

    docs: added docs page

---
+++ docs/src/scripts/index.js
@@ -0,0 +1,8 @@
+import {Keukenhof} from 'keukenhof';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-bash';
+
+Keukenhof.init({
+    hasAnimation: true,
+});
